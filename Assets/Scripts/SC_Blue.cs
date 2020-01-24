ca0ee4e45d946bbe597e56ab9ac46f055e52fd88
yianni <yianman26@gmail.com>
2020-01-24 14:29:20 -0800

    -ranges to disappear if too far

--- Assets/Scripts/SC_Blue.cs
+++ Assets/Scripts/SC_Blue.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class SC_Blue : MonoBehaviour
 {
+    public GameObject board;
+
+
+    private void Start()
+    {
+        board.SetActive(false);
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag.Contains("Ball"))
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            board.SetActive(true);
+        }
+        else
+        {
+            board.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Contains("Ball"))
         {
             Destroy(gameObject);
         }
     }
+
+
 }
